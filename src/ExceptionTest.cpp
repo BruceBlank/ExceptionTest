faceda6d05f10d7d41f1dd1fb63f4be343fd12ee
Bruce Blank <Torsten.Blank@gmail.com>
2012-07-12 20:45:35 +0000

    bad_exception an unexpected_handler added

--- src/ExceptionTest.cpp
+++ src/ExceptionTest.cpp
@@ -7,9 +7,10 @@
 //============================================================================
 
 #include <iostream>
+#include <exception>
 using namespace std;
 
-class ExceptA {
+class ExceptA : public exception {
 
 };
 
@@ -19,17 +20,56 @@ class ExceptB {
 
 void func1() {
 	cout << "In func1" << endl;
+	throw ExceptA();
 }
 
-void func2() {
+void func2() throw (std::bad_exception, ExceptA){
 	cout << "In func2" << endl;
+	throw ExceptB();
 }
 
+void my_unexpected ()
+{
+    if (!std::uncaught_exception())
+        std::cerr << "my_unexpected called\n";
+    throw; // now its a bad_exception
+}
 
 int main() {
 	cout << "BEGIN" << endl;
-	func1();
-	func2();
 
+	// try func1
+	try{
+		func1();
+	}
+	catch(ExceptA){
+		cout << "ExceptA caught!" << endl;
+	}
+
+	// try func2 with new unexpected handler
+	std::unexpected_handler oldhandler = std::set_unexpected(my_unexpected);
+	try{
+		func2();
+	}
+	catch(ExceptA){
+		cout << "ExceptA caught!" << endl;
+	}
+	catch(std::bad_exception){
+		cout << "Bad Exception caught" << endl;
+	}
+
+	// try func2 with old unexpected handler
+	std::set_unexpected(oldhandler);
+	try{
+		func2();
+	}
+	catch(ExceptA){
+		cout << "ExceptA caught!" << endl;
+	}
+	catch(std::bad_exception){
+		cout << "Bad Exception caught" << endl;
+	}
+
+	cout << "END" << endl;
 	return 0;
 }
